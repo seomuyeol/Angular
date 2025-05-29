8bbf151aa36144ae4dc97185c5480cb5976c8888
seomuyeol <muyear.seo@gmail.com>
2025-05-29 22:59:20 +0900

    250529

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
+import { HomeComponent } from './app/home/home.component';
 
 @Component({
   selector: 'app-root',
+  imports: [HomeComponent,],
   template: `
-    <h1>Hello from {{ name }}!</h1>
-    <a target="_blank" href="https://angular.dev/overview">
-      Learn more about Angular
-    </a>
+  <h1>Hello World!</h1>
+  <main>
+    <header class="brand-name">
+      <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
+    </header>
+
+    <section class="content">
+      <app-home></app-home>
+    </section>
+  </main>
   `,
 })
 export class App {
